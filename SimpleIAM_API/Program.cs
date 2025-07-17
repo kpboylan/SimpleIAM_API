04ac36f14697f04309de7e9275312fd6d745af2b
kpboylan <kboylan@gmail.com>
2025-07-17 15:59:56 +0100

    Enable Swagger in Azure

--- SimpleIAM_API/Program.cs
+++ SimpleIAM_API/Program.cs
@@ -52,11 +52,8 @@ using (var scope = app.Services.CreateScope())
 }
 
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
